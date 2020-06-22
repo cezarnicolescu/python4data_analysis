b57edcb6d391fcb12bbb40df1dbae985fa8d28df
cezarnicolescu <cezarnicolescu@icloud.co>
2020-06-22 17:45:45 +0200

    #create multidimensional array

--- numpy_arrays.py
+++ numpy_arrays.py
@@ -4,3 +4,11 @@ import numpy as np
 my_list = [1,2,3,4]
 my_np_array = np.array(my_list)
 print(my_np_array)
+
+#create multidimensional array
+
+my_2nd_list = ["a", "b", "c", "c"]
+
+lists = [my_2nd_list, my_list]
+matrix_array = np.array(lists)
+print(matrix_array)
